f41e37a93f19cf2b67af93deaf6cea8af31df793
sathishcaliber <sathish@caliberfocus.com>
2023-12-13 14:02:32 +0530

    kernel file updated

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         $schedule->call(function (AccessCardController $accessCardController) {
             $accessCardController->insertLiveData();
-        })->everyThirtyMinutes();
+        })->everyMinute();
         // $schedule->call([AccessCardController::class, 'insertLiveData'])->everyMinute();
     }
 
